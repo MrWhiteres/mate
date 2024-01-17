87d7be83c0222d4eda99396a3c93c84dabb283a4
MrWhiteres <alexandr.leluk.99@gmail.com>
2024-01-17 21:52:46 +0200

    work

--- mate.js
+++ mate.js
@@ -1,17 +1,32 @@
-function countMatchingSocks(colors) {
-  const sockCounts = {};
-  let pairs = 0;
+'use strict';
 
-  for (const color of colors) {
-    // Якщо кольор уже зустрічався, збільшуємо лічильник та перевіряємо, чи можна створити нову пару
-    if (sockCounts[color]) {
-      pairs += 1;
-      sockCounts[color] = 0;
-    } else {
-      // Якщо кольор ще не зустрічався, додаємо його до лічильника
-      sockCounts[color] = 1;
+/**
+ * @param {string} word
+ *
+ * @returns {string}
+ */
+function encodeDuplicates(word) {
+  const charCount = {};
+
+  for (const char of word.toLowerCase()) {
+    if (char.match(/[a-z]/)) {
+      charCount[char] = (charCount[char] || 0) + 1;
     }
   }
 
-  return pairs;
+  let result = '';
+
+  for (const char of word.toLowerCase()) {
+    if (char.match(/[a-z]/)) {
+      if (charCount[char] === 1) {
+        result += 'x';
+      } else {
+        result += 'y';
+      }
+    } else {
+      result += char;
+    }
+  }
+
+  return result;
 }
---
+++ react_demo/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "./components/header/Header";
+import "./App.scss";
+
+export const App = () => {
+  return (
+    <div>
+      <div className="App">Hello World</div>
+      <Header />
+    </div>
+  );
+};
